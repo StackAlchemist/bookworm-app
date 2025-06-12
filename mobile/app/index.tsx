dbfb84dc7b511ad5b6e7e19dbae2f807f1e8ffab
StackAlchemist <jolamideprince@gmail.com>
2025-06-12 23:59:50 +0100

    app setup

---
+++ mobile/app/index.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Image } from "expo-image";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.title}>Hello</Text>
+      <Link href="/signup">Sign Up</Link>
+      <Link href="/login">Log in</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title:{
+    color: "blue"
+  }
+})
